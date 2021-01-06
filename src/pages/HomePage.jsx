cb35dd7143abe3bd044345a2b30397a4a8f4e4df
yuchifang <42566280+yuchifang@users.noreply.github.com>
2021-01-07 00:24:32 +0800

    update__err

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,39 +7,41 @@ import { heroData } from "../data/HeroData";
 const SHUTDOWN_RATE = 0.2;
 
 const HomePage = () => {
-  const [initialData, setInitialData] = useState()
+  const [initialData, setInitialData] = useState();
 
-  const [webStatus, setWebStatus] = useState("idle")
+  const [webStatus, setWebStatus] = useState("idle");
 
   useEffect(() => {
     getData()
       .then((res) => {
-        setInitialData(res)
-        setWebStatus("success")
+        setInitialData(res);
+        setWebStatus("success");
       })
       .catch((err) => {
-        setWebStatus("error")
-      })
-
-  }, [])
+        setWebStatus("error");
+      });
+  }, []);
 
   const getData = async () => {
     if (Math.random() < SHUTDOWN_RATE) {
-      throw new Error("模擬錯誤 請重新整理")
+      throw new Error("模擬錯誤 請重新整理");
     }
-    return heroData
-  }
+    return heroData;
+  };
 
-  return (<>
-    { webStatus === "success" && <BrowserRouter>
-      <HeroBlock initialData={initialData} />
-      <Switch>
-        <TalentBlock />
-      </Switch>
-    </BrowserRouter>
-    }
-    {webStatus === "loading" || webStatus === "idle" && <h1>Loading</h1>}
-    {webStatus === "error" && <h1>模擬錯誤 請重新整理</h1>}
-  </>)
+  return (
+    <>
+      {webStatus === "success" && (
+        <BrowserRouter>
+          <HeroBlock initialData={initialData} />
+          <Switch>
+            <TalentBlock />
+          </Switch>
+        </BrowserRouter>
+      )}
+      {(webStatus === "loading" || webStatus === "idle") && <h1>Loading</h1>}
+      {webStatus === "error" && <h1>模擬錯誤 請重新整理</h1>}
+    </>
+  );
 };
 export default HomePage;
