31bab192b269eb993c72c5d9f0dd7d3c67564e95
fangfelipe <fangfelipe@gmail.com>
2021-01-10 21:58:48 +0800

    update__delay and errorDom and loadingDom

--- src/HomePageApp.jsx
+++ src/HomePageApp.jsx
@@ -3,6 +3,9 @@ import HeroBlock from "./pages/HeroBlock";
 import TalentBlock from "./pages/TalentBlock";
 import { BrowserRouter, Switch } from "react-router-dom";
 import heroData from "./data/HeroData.json";
+import "antd/dist/antd.css"
+import { Alert } from "antd";
+import { Spinner, Jumbotron, Container, Row } from 'react-bootstrap';
 
 const SHUTDOWN_RATE = 0.2;
 
@@ -22,13 +25,17 @@ const HomePage = () => {
       });
   }, []);
 
+  // const delay = (ms) => {
+  //   return new Promise((resolve) => setTimeout(resolve, ms));
+  // }
+
   const getData = async () => {
+    const a = await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000 + 10000));
     if (Math.random() < SHUTDOWN_RATE) {
       throw new Error("模擬錯誤 請重新整理");
     }
     return heroData;
   };
-
   return (
     <>
       {webStatus === "success" && (
@@ -39,8 +46,15 @@ const HomePage = () => {
           </Switch>
         </BrowserRouter>
       )}
-      {(webStatus === "loading" || webStatus === "idle") && <h1>Loading</h1>}
-      {webStatus === "error" && <h1>模擬錯誤 請重新整理</h1>}
+      {(webStatus === "loading" || webStatus === "idle") &&
+        <Container>
+          <Row className="justify-content-center">
+            <Jumbotron>
+              <Spinner width="90%" animation="border" />
+            </Jumbotron>
+          </Row>
+        </Container>}
+      {webStatus === "error" && <Alert description="模擬錯誤 請重新整理" message="Error" type="error" showIcon />}
     </>
   );
 };
