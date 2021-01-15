d6f26637b0426aefbca8344dedf35ccf57b78247
fangfelipe <fangfelipe@gmail.com>
2021-01-15 14:17:51 +0800

    update errorTest

--- src/HomePageApp.jsx
+++ src/HomePageApp.jsx
@@ -40,9 +40,9 @@ const HomePage = () => {
     const a = await new Promise((resolve) =>
       setTimeout(resolve, Math.random() * 1000 + 1000)
     );
-    // if (Math.random() < SHUTDOWN_RATE) {
-    //   throw new Error("模擬錯誤 請重新整理");
-    // }
+    if (Math.random() < SHUTDOWN_RATE) {
+      throw new Error("模擬錯誤 請重新整理");
+    }
     return heroData;
   };
   return (
